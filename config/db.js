e5611ab9ce1e719308b011cf5917fbdc0bd7cef1
daan98 <dann98.da@gmail.com>
2022-08-28 14:27:04 -0500

    db.js port changed

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv/config'
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    port: '3308',
+    port: '3306',
     dialect: 'mysql',
     define: {
         timestamps: false,
